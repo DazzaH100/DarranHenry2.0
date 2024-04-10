e43522a807688b5f5c03d7c3c769a43962e5911b
DazzaH100 Henry <henrydarran@yahoo.com>
2024-04-10 13:53:26 +0000

    experiment

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// Initialize Particles.js
+document.addEventListener('DOMContentLoaded', function() {
+    particlesJS.load('particles-js', 'particles.json', function() {
+      console.log('callback - particles.js config loaded');
+    });
+  });
+  
---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    particlesJS('particles-js', {
+        particles: {
+            number: {
+                value: 80,
+                density: {
+                    enable: true,
+                    value_area: 800
+                }
+            },
+            color: {
+                value: ['#000000', '#6A82FB', '#A4EBF3', '#ffe']
+            },
+            // ...additional configurations...
+        },
+        // ...other particles.js settings...
+    });
+});
